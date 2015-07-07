bbffa055576ae87126d844662363765502074d15
Paul Gladkov <webgeist.xor@gmail.com>
2015-07-08 01:01:35 +0300

    d

---
+++ build.sbt
@@ -0,0 +1,8 @@
+name := "HarmonicCentrality"
+
+version := "1.0"
+
+scalaVersion := "2.11.7"
+
+libraryDependencies += "com.twitter" %% "algebird-core" % "0.10.2"
+libraryDependencies += "org.scalatest" % "scalatest_2.11" % "2.2.4" % "test"
---
+++ src/test/scala/HarmonicCentrality/HarmonicCentralityTest.scala
@@ -0,0 +1,15 @@
+package HarmonicСentrality
+
+import org.scalatest._
+import org.scalatest.FunSuite
+
+class HarmonicCentralityTest extends FunSuite with ShouldMatchers {
+
+  test("first") {
+    assert(true)
+  }
+
+  test("second") {
+    assert(true)
+  }
+}
