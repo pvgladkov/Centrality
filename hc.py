9d148f5df061dec4ed6bd19bd4c6ac07d922956e
Paul Gladkov <webgeist.xor@gmail.com>
2015-07-11 11:55:06 +0300

    hc

--- hc.py
+++ hc.py
@@ -1,34 +1,34 @@
-import networkx
-from collections import defaultdict
 from networkx import Graph
-from harmonic_centrality import harmonic_centrality
-
-def harmonic_centrality_set(G, max_distance=6):
-    harmonic = defaultdict(lambda: 0.)
-    t_steps_set = defaultdict(lambda: defaultdict(lambda: set()))
-    for node in G.nodes_iter():
-        t_steps_set[node][0].add(str(node))
-    for distance in range(1, max_distance + 1):
-        for node in G.nodes_iter():
-            t_steps_set[node][distance].update(t_steps_set[node][distance-1])
-            for next_node in networkx.all_neighbors(G, node):
-                t_steps_set[node][distance].update(t_steps_set[next_node][distance - 1])
-            harmonic[node] += float((len(t_steps_set[node][distance]) - len(t_steps_set[node][distance - 1])))/distance
-    return dict(harmonic)
-
-G = Graph()
-G.add_edge(1,2)
-G.add_edge(1,3)
-G.add_edge(1,4)
-G.add_edge(2,3)
-harmonic = harmonic_centrality_set(G)
-harmonic_hll = harmonic_centrality(G)
+from networkx.algorithms.centrality import harmonic_centrality
+from harmonic_centrality import harmonic_centrality as hc_hll
 
 
-print "set"
-for node in harmonic:
-    print node, harmonic[node]
+def comp(graph):
+    harmonic = harmonic_centrality(graph)
+    harmonic_hll = hc_hll.harmonic_centrality(graph)
+    print("networkx")
+    for node in harmonic:
+        print(node, harmonic[node])
 
-print "\nhll"
-for node in harmonic_hll:
-    print node, harmonic[node]
+    print("\nhll")
+    for node in harmonic_hll:
+        print(node, harmonic[node])
+
+if __name__ == "__main__":
+
+    G = Graph()
+    G.add_edge(1, 2)
+    G.add_edge(1, 3)
+    G.add_edge(1, 4)
+    G.add_edge(2, 3)
+    comp(G)
+
+    G = Graph()
+    G.add_edge(1, 2)
+    G.add_edge(2, 3)
+    G.add_edge(3, 4)
+    G.add_edge(2, 3)
+    G.add_edge(2, 4)
+    G.add_edge(5, 1)
+    comp(G)
+
--- src/test/scala/hc/HarmonicCentralityTest.scala
+++ src/test/scala/hc/HarmonicCentralityTest.scala
@@ -7,17 +7,21 @@ import scalax.collection.Graph
 import scalax.collection.GraphEdge._
 import scalax.collection.GraphPredef._
 
-class HarmonicCentralityTest extends FunSuite with ShouldMatchers {
+class HarmonicCentralityTest extends Suite {
 
-  test("first") {
+  def testGraph1(info: Informer) {
     val g = Graph[Int, HyperEdge](1~2, 1~3, 1~4, 2~3)
     val res = HarmonicCentrality(g)
     for ((k,v) <- res.iterator){
-      println(k,v)
+      info((k,v).toString())
     }
   }
 
-  test("second") {
-    assert(true)
+  def testGraph2(info: Informer) {
+    val g = Graph[Int, HyperEdge](1~2, 2~3, 3~4, 2~4, 5~1)
+    val res = HarmonicCentrality(g)
+    for ((k,v) <- res.iterator){
+      info((k,v).toString())
+    }
   }
 }
